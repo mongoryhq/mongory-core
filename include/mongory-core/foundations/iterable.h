bd742443c75a542989c00af1fdac358d8c720d9f
Frank Liao <koten0224@gmail.com>
2025-05-03 05:58:34 +0800

    feat(iterable): implement iterable functions

--- include/mongory-core/foundations/iterable.h
+++ include/mongory-core/foundations/iterable.h
@@ -2,6 +2,7 @@
 #define MONGORY_ITER
 #include <stdbool.h>
 #include <stdlib.h>
+#include <mongory-core/foundations/memory_pool.h>
 
 struct mongory_iterable;
 typedef struct mongory_iterable mongory_iterable;
@@ -13,6 +14,11 @@ struct mongory_iterable {
   size_t count;
   void **items;
 };
+
+mongory_iterable* mongory_iterable_new(mongory_memory_pool *pool);
+bool mongory_iterable_push(mongory_iterable *iter, mongory_memory_pool *pool, void *item);
+void* mongory_iterable_get(mongory_iterable *iter, size_t index);
+bool mongory_iterable_set(mongory_iterable *iter, mongory_memory_pool *pool, size_t index, void *item);
 
 inline bool mongory_iterable_each(mongory_iterable *self, void *acc, mongory_iterable_callback_func func) {
   for (size_t i = 0; i < self->count; i++) {
--- src/foundations/iterable.c
+++ src/foundations/iterable.c
@@ -1,1 +1,74 @@
 #include <mongory-core/foundations/iterable.h>
+#define MONGORY_ITERABLE_INIT_SIZE 4
+
+mongory_iterable* mongory_iterable_new(mongory_memory_pool *pool) {
+  void **items = pool->alloc(pool, sizeof(void *) * MONGORY_ITERABLE_INIT_SIZE);
+  if (!items) {
+    return NULL;
+  }
+  mongory_iterable *iter = pool->alloc(pool, sizeof(mongory_iterable));
+  if (!iter) {
+    return NULL;
+  }
+
+  iter->items = items;
+  iter->capacity = MONGORY_ITERABLE_INIT_SIZE;
+  iter->count = 0;
+
+  return iter;
+}
+
+inline bool mongory_iterable_grow_if_needed(mongory_iterable *iter, mongory_memory_pool *pool, size_t size) {
+  if (size < iter->capacity) {
+    return true;
+  }
+
+  size_t new_capacity = iter->capacity * 2;
+  while (new_capacity <= size) {
+    new_capacity *= 2;
+  }
+  void **new_items = pool->alloc(pool, sizeof(void *) * new_capacity);
+  if (!new_items) {
+    return false;
+  }
+
+  for (size_t i = 0; i < iter->count; ++i) {
+    new_items[i] = iter->items[i];
+  }
+
+  iter->items = new_items;
+  iter->capacity = new_capacity;
+  return true;
+}
+
+bool mongory_iterable_push(mongory_iterable *iter, mongory_memory_pool *pool, void *item) {
+  if (!mongory_iterable_grow_if_needed(iter, pool, iter->count)) {
+    return false;
+  }
+
+  iter->items[iter->count++] = item;
+  return true;
+}
+
+void* mongory_iterable_get(mongory_iterable *iter, size_t index) {
+  if (index >= iter->count) {
+    return NULL;
+  }
+  return iter->items[index];
+}
+
+bool mongory_iterable_set(mongory_iterable *iter, mongory_memory_pool *pool, size_t index, void *item) {
+  if (!mongory_iterable_grow_if_needed(iter, pool, index + 1)) {
+    return false;
+  }
+
+  if (index >= iter->count) {
+    for (size_t i = iter->count; i < index; ++i) {
+      iter->items[i] = NULL;
+    }
+    iter->count = index + 1;
+  }
+
+  iter->items[index] = item;
+  return true;
+}
