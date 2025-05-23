c940c3148f5b6e8ca2d3e0bff490061937cce79c
frank0224 <frank@shopline.com>
2025-05-24 03:57:30 +0800

    chore: remove trailing white space

--- scripts/setup_unity.sh
+++ scripts/setup_unity.sh
@@ -8,4 +8,4 @@ curl -o tests/unity/unity.h https://raw.githubusercontent.com/ThrowTheSwitch/Uni
 curl -o tests/unity/unity.c https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity.c
 curl -o tests/unity/unity_internals.h https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity_internals.h
 
-echo "Unity test framework has been set up successfully!" 
+echo "Unity test framework has been set up successfully!"
