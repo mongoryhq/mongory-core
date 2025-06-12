c2337408943217b873afbf6e1209b24b0d376fef
Frank Liao <koten0224@gmail.com>
2025-06-13 05:22:35 +0800

    test: extend config test

--- tests/mongory_config_test.c
+++ tests/mongory_config_test.c
@@ -3,10 +3,12 @@
 #include "unity.h"
 #include <mongory-core.h>
 #include "../src/foundations/config_private.h"
+#include "../src/matchers/inclusion_matcher.h"
 
 mongory_memory_pool *pool;
 
 void setUp(void) {
+    mongory_init();
     pool = mongory_memory_pool_new();
     TEST_ASSERT_NOT_NULL(pool);
 }
@@ -16,6 +18,7 @@ void tearDown(void) {
         pool->free(pool);
         pool = NULL;
     }
+    mongory_cleanup();
 }
 
 void test_mongory_init(void) {
@@ -27,7 +30,7 @@ void test_mongory_cleanup(void) {
     mongory_cleanup();
     TEST_ASSERT_NULL(mongory_internal_pool);
     TEST_ASSERT_NULL(mongory_internal_regex_adapter);
-    mongory_init();
+    TEST_ASSERT_NULL(mongory_matcher_mapping);
 }
 
 static bool test_regex_func(mongory_memory_pool *pool, mongory_value *pattern, mongory_value *value) {
@@ -58,23 +61,42 @@ void test_mongory_matcher_register(void) {
     TEST_ASSERT_EQUAL(test_build_func, retrieved);
 }
 
+bool print_matcher_mapping(const char *key, mongory_value *value, void *acc) {
+    (void)acc;
+    printf("key: %s, value: %p\n", key, value);
+    return true;
+}
+
 void test_mongory_matcher_build_func_get(void) {
     TEST_ASSERT_NOT_NULL(mongory_matcher_mapping);
-    mongory_matcher_build_func in_matcher = mongory_matcher_build_func_get("$in");
-    TEST_ASSERT_NOT_NULL(in_matcher);
-
-    mongory_matcher_build_func unknown_matcher = mongory_matcher_build_func_get("$unknown");
-    TEST_ASSERT_NULL(unknown_matcher);
+    
+    mongory_matcher_mapping->each(mongory_matcher_mapping, NULL, print_matcher_mapping);
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$in"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$nin"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$eq"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$ne"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$gt"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$gte"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$lt"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$lte"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$exists"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$present"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$regex"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$and"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$or"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$elemMatch"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$every"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$not"));
+    TEST_ASSERT_NOT_NULL(mongory_matcher_build_func_get("$size"));
+    TEST_ASSERT_NULL(mongory_matcher_build_func_get("$unknown"));
 }
 
 int main(void) {
     UNITY_BEGIN();
-    mongory_init();
     RUN_TEST(test_mongory_init);
     RUN_TEST(test_mongory_cleanup);
     RUN_TEST(test_mongory_regex_func_set);
     RUN_TEST(test_mongory_matcher_register);
     RUN_TEST(test_mongory_matcher_build_func_get);
-    mongory_cleanup();
     return UNITY_END();
 }
